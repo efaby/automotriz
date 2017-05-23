60796ad40f79499114e47e8ba98743dfdad04e64
Faby <efaby10@gmail.com>
2017-05-23 17:08:10 -0500

    Ajuste template

--- public/template/header.php
+++ public/template/header.php
@@ -28,65 +28,12 @@
     			<i class="fa fa-bars"></i>
     		</button> </div>
                     <div class="header-block header-block-search hidden-sm-down">
-                        <form role="search">
-                            <div class="input-container"> <i class="fa fa-search"></i> <input type="search" placeholder="Search">
-                                <div class="underline"></div>
-                            </div>
-                        </form>
+                        
                     </div>
-                    <div class="header-block header-block-buttons">
-                        <a href="https://github.com/modularcode/modular-admin-html" class="btn btn-sm header-btn"> <i class="fa fa-github-alt"></i> <span>View on GitHub</span> </a>
-                        <a href="https://github.com/modularcode/modular-admin-html/stargazers" class="btn btn-sm header-btn"> <i class="fa fa-star"></i> <span>Star Us</span> </a>
-                        <a href="https://github.com/modularcode/modular-admin-html/releases/download/v1.0.1/modular-admin-html-1.0.1.zip" class="btn btn-sm header-btn"> <i class="fa fa-cloud-download"></i> <span>Download .zip</span> </a>
-                    </div>
+                    
                     <div class="header-block header-block-nav">
                         <ul class="nav-profile">
-                            <li class="notifications new">
-                                <a href="" data-toggle="dropdown"> <i class="fa fa-bell-o"></i> <sup>
-    			      <span class="counter">8</span>
-    			    </sup> </a>
-                                <div class="dropdown-menu notifications-dropdown-menu">
-                                    <ul class="notifications-container">
-                                        <li>
-                                            <a href="" class="notification-item">
-                                                <div class="img-col">
-                                                    <div class="img" style="background-image: url('assets/faces/3.jpg')"></div>
-                                                </div>
-                                                <div class="body-col">
-                                                    <p> <span class="accent">Zack Alien</span> pushed new commit: <span class="accent">Fix page load performance issue</span>. </p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="notification-item">
-                                                <div class="img-col">
-                                                    <div class="img" style="background-image: url('assets/faces/5.jpg')"></div>
-                                                </div>
-                                                <div class="body-col">
-                                                    <p> <span class="accent">Amaya Hatsumi</span> started new task: <span class="accent">Dashboard UI design.</span>. </p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="" class="notification-item">
-                                                <div class="img-col">
-                                                    <div class="img" style="background-image: url('assets/faces/8.jpg')"></div>
-                                                </div>
-                                                <div class="body-col">
-                                                    <p> <span class="accent">Andy Nouman</span> deployed new version of <span class="accent">NodeJS REST Api V3</span> </p>
-                                                </div>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <footer>
-                                        <ul>
-                                            <li> <a href="">
-    			            View All
-    			          </a> </li>
-                                        </ul>
-                                    </footer>
-                                </div>
-                            </li>
+                            
                             <li class="profile dropdown">
                                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                                     <div class="img" style="background-image: url('https://avatars3.githubusercontent.com/u/3959008?v=3&s=40')"> </div> <span class="name">
@@ -109,42 +56,29 @@
                             <div class="brand">
                                 <div class="logo"> <span class="l l1"></span> <span class="l l2"></span> <span class="l l3"></span> <span class="l l4"></span> <span class="l l5"></span> </div> Modular Admin </div>
                         </div>
+                        <?php $url = $_SERVER["REQUEST_URI"];?>
                         <nav class="menu">
                             <ul class="nav metismenu" id="sidebar-menu">
-                                <li class="active">
-                                    <a href="index.html"> <i class="fa fa-home"></i> Dashboard </a>
+                                <li class="<?php echo (strpos($url, '/Seguridad/inicio/'))?'active':'';?>">
+                                    <a href="../../Seguridad/inicio/"> <i class="fa fa-home"></i> Inicio </a>
                                 </li>
-                                <li>
-                                    <a href=""> <i class="fa fa-th-large"></i> Items Manager <i class="fa arrow"></i> </a>
-                                    <ul>
-                                        <li> <a href="items-list.html">
-    								Items List
-    							</a> </li>
-                                        <li> <a href="item-editor.html">
-    								Item Editor
-    							</a> </li>
-                                    </ul>
+                                <li class="<?php echo (strpos($url, '/Usuario/listar/'))?'active':'';?>">
+                                    <a href="../../Usuario/listar/"> <i class="fa fa-users"></i> Usuarios </a>                                    
                                 </li>
-                                <li>
-                                    <a href=""> <i class="fa fa-bar-chart"></i> Charts <i class="fa arrow"></i> </a>
-                                    <ul>
-                                        <li> <a href="charts-flot.html">
-    								Flot Charts
-    							</a> </li>
-                                        <li> <a href="charts-morris.html">
-    								Morris Charts
-    							</a> </li>
-                                    </ul>
+                                <li class="<?php echo (strpos($url, '/Vehiculo/listar/'))?'active':'';?>">
+                                    <a href="../../Vehiculo/listar/"> <i class="fa fa-truck"></i> Vehículos </a>
+                                    
                                 </li>
-                                <li>
-                                    <a href=""> <i class="fa fa-table"></i> Tables <i class="fa arrow"></i> </a>
+                                
+                                <li class="<?php echo (strpos($url, '/Plan/listar/'))?'active':'';?>">
+                                    <a href=""> <i class="fa fa-book"></i> Mantenimiento <i class="fa arrow"></i> </a>
                                     <ul>
-                                        <li> <a href="static-tables.html">
-    								Static Tables
-    							</a> </li>
-                                        <li> <a href="responsive-tables.html">
-    								Responsive Tables
-    							</a> </li>
+                                        <li> <a href="../../Plan/listar/">
+        								Planes
+        						          </a> </li>
+                                            <li> <a href="responsive-tables.html">
+        								Asignar
+        							</a> </li>
                                     </ul>
                                 </li>
                                 <li>
@@ -202,67 +136,7 @@
                             </ul>
                         </nav>
                     </div>
-                    <footer class="sidebar-footer">
-                        <ul class="nav metismenu" id="customize-menu">
-                            <li>
-                                <ul>
-                                    <li class="customize">
-                                        <div class="customize-item">
-                                            <div class="row customize-header">
-                                                <div class="col-xs-4"> </div>
-                                                <div class="col-xs-4"> <label class="title">fixed</label> </div>
-                                                <div class="col-xs-4"> <label class="title">static</label> </div>
-                                            </div>
-                                            <div class="row hidden-md-down">
-                                                <div class="col-xs-4"> <label class="title">Sidebar:</label> </div>
-                                                <div class="col-xs-4"> <label>
-    				                        <input class="radio" type="radio" name="sidebarPosition" value="sidebar-fixed" >
-    				                        <span></span>
-    				                    </label> </div>
-                                                <div class="col-xs-4"> <label>
-    				                        <input class="radio" type="radio" name="sidebarPosition" value="">
-    				                        <span></span>
-    				                    </label> </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-xs-4"> <label class="title">Header:</label> </div>
-                                                <div class="col-xs-4"> <label>
-    				                        <input class="radio" type="radio" name="headerPosition" value="header-fixed">
-    				                        <span></span>
-    				                    </label> </div>
-                                                <div class="col-xs-4"> <label>
-    				                        <input class="radio" type="radio" name="headerPosition" value="">
-    				                        <span></span>
-    				                    </label> </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-xs-4"> <label class="title">Footer:</label> </div>
-                                                <div class="col-xs-4"> <label>
-    				                        <input class="radio" type="radio" name="footerPosition" value="footer-fixed">
-    				                        <span></span>
-    				                    </label> </div>
-                                                <div class="col-xs-4"> <label>
-    				                        <input class="radio" type="radio" name="footerPosition" value="">
-    				                        <span></span>
-    				                    </label> </div>
-                                            </div>
-                                        </div>
-                                        <div class="customize-item">
-                                            <ul class="customize-colors">
-                                                <li> <span class="color-item color-red" data-theme="red"></span> </li>
-                                                <li> <span class="color-item color-orange" data-theme="orange"></span> </li>
-                                                <li> <span class="color-item color-green active" data-theme=""></span> </li>
-                                                <li> <span class="color-item color-seagreen" data-theme="seagreen"></span> </li>
-                                                <li> <span class="color-item color-blue" data-theme="blue"></span> </li>
-                                                <li> <span class="color-item color-purple" data-theme="purple"></span> </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                </ul>
-                                <a href=""> <i class="fa fa-cog"></i> Customize </a>
-                            </li>
-                        </ul>
-                    </footer>
+                    
                 </aside>
                 <div class="sidebar-overlay" id="sidebar-overlay"></div>
                 <article class="content dashboard-page">
