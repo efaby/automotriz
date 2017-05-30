9af08fe55d53fac470ec3f6a4e287b4e2c63626b
Faby <efaby10@gmail.com>
2017-05-30 15:29:07 -0500

    avatar

--- public/template/header.php
+++ public/template/header.php
@@ -37,7 +37,7 @@
                             
                             <li class="profile dropdown">
                                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <div class="img" style="background-image: url('https://avatars3.githubusercontent.com/u/3959008?v=3&s=40')"> </div> <span class="name">
+                                    <div class="img" style="background-image: url('<?php echo PATH_IMAGES; ?>/avatar_m.png')"> </div> <span class="name">
     			      John Doe
     			    </span> </a>
                                 <div class="dropdown-menu profile-dropdown-menu" aria-labelledby="dropdownMenu1">
