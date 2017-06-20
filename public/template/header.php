01ac84bee3bf6f944a3de2e7c9b046faef5a3280
efaby10 <efaby10@gmail.com>
2017-06-19 23:18:24 -0500

    cambios

--- public/template/header.php
+++ public/template/header.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title> ModularAdmin - Free Dashboard Theme | HTML Version </title>
+        <title>Mantenimiento Automotriz </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
