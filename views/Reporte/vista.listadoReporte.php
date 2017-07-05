d4f5fdef23274dc59f457c5855f48b3626e33d02
Jane <lajane2020@hotmail.com>
2017-07-05 00:35:54 -0500

    Cambio en el título

--- views/Reporte/vista.listadoReporte.php
+++ views/Reporte/vista.listadoReporte.php
@@ -104,7 +104,7 @@
 		<div class="modal-content" style="width:984px">
 			<div class="modal-header">
 				<a class="close" data-dismiss="modal">×</a>
-				<h3>Historial de Detalle de Fallas de Mantenimiento</h3>
+				<h3>Detalle de Fallas de Mantenimiento</h3>
 			</div>
 			<div class="modal-body"></div>
 		</div>
