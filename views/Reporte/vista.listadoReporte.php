60c1ee34890f6ccad092abba0ffe5d78385dcb55
efaby10 <efaby10@gmail.com>
2017-07-05 20:55:04 -0500

    modal size

--- views/Reporte/vista.listadoReporte.php
+++ views/Reporte/vista.listadoReporte.php
@@ -100,8 +100,8 @@
 </div>
 <div class="modal fade" id="confirm-submit" tabindex="-1" role="dialog" 
 	aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-dialog" style="width: 1301px;">
-		<div class="modal-content" style="width:984px">
+	<div class="modal-dialog  modal-lg" >
+		<div class="modal-content">
 			<div class="modal-header">
 				<a class="close" data-dismiss="modal">×</a>
 				<h3>Detalle de Fallas de Mantenimiento</h3>
